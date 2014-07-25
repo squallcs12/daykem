36cec337c02d34aafe6503ea0e828256c8dcd61d
Bang Dao <antipro.vn@gmail.com>
2014-07-25 17:15:08 +0700

    fix google auto login redirect value

--- accounts/templatetags/back_url_tag.py
+++ accounts/templatetags/back_url_tag.py
@@ -5,29 +5,32 @@ Created on Jul 29, 2013
 '''
 from django import template
 from django.contrib.auth import REDIRECT_FIELD_NAME
+
 from common.django_custom import get_go_back_url
 
 register = template.Library()
 
-css_files = []
-js_files = []
-
 
 @register.tag
-def back_url(parser, token):
+def back_url(_, token):
     try:
-        tag_name, default = token.split_contents()  # @UnusedVariable
-        return BackUrlNode(default=default)
+        args = token.split_contents()  # @UnusedVariable
+        return BackUrlNode(*args)
     except ValueError:
         return BackUrlNode()
 
 
 class BackUrlNode(template.Node):
-    def __init__(self, default=None, redirect_field_name=REDIRECT_FIELD_NAME):
+    def __init__(self, default=None, prepend=None, redirect_field_name=REDIRECT_FIELD_NAME):
+        self._prepend = prepend
         self._default = default
         self._redirect_field_name = redirect_field_name
 
     def render(self, context):
         request = context['request']
-        backUrl = get_go_back_url(request, self._redirect_field_name, self._default)
-        return '' if not backUrl else '?next=%s' % backUrl
+        back_href = get_go_back_url(request, self._redirect_field_name, self._default)
+
+        if self._prepend == '0':
+            return back_href
+        return '' if not back_href else '?next=%s' % back_href
+
